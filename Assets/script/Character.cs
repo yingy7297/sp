1d959f4e09c35eb2aea2aa788457115c278fd018
tsaibird <yingy7297@gmail.com>
2026-03-17 16:01:00 +0800

    角色重力腳本
    更改#C新增角色重力、地圖些微調整

--- Assets/script/Character.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Character
-{
-    float h = Input.GetAxis("Horizontal");
-}
---
+++ Assets/script/ControlSystem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ControlSystem : MonoBehaviour 
+{
+    [Header("基本數值")]
+    [SerializeField, Range(0, 10)]
+    private float movespeed = 5f;
+    [Header("元件")]
+    [SerializeField]
+    private Rigidbody2D rig;
+}
